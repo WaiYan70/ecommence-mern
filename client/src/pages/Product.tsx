9ebc1c812697801406ebfe4eae0c3f1d2c3c126b
Khant Wai Yan <52145576+WaiYan70@users.noreply.github.com>
2024-12-12 21:03:20 +0700

    Finish Single Product Page

--- client/src/pages/Product.tsx
+++ client/src/pages/Product.tsx
@@ -1,17 +1,18 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
-import { Product as ProductType } from "../assets/assets";
+import { icons, Product as ProductType } from "../assets/assets";
 
 const Product: React.FC = () => {
   const context = useContext(ShopContext);
   if (!context) {
     throw new Error("ShopContext must be used within a ShopContextProvider");
   }
-  const { products } = context;
+  const { products, currency } = context;
   const { productId } = useParams();
   const [productData, setProductData] = useState<ProductType | null>(null);
   const [image, setImage] = useState("");
+  const [size, setSize] = useState("");
 
   const fetchProductData = () => {
     const foundProduct = products.find((item) => item._id === productId);
@@ -49,6 +50,63 @@ const Product: React.FC = () => {
         {/* Product Info */}
         <div className="flex-1">
           <h1 className="font-medium text-2xl mt-2">{productData.title}</h1>
+          <div className="flex items-center gap-1 mt-2">
+            <img src={icons.star_icon} alt="" className="w-3 5" />
+            <img src={icons.star_icon} alt="" className="w-3 5" />
+            <img src={icons.star_icon} alt="" className="w-3 5" />
+            <img src={icons.star_icon} alt="" className="w-3 5" />
+            <img src={icons.star_dull_icon} alt="" className="w-3 5" />
+            <p className="pl-2">(122)</p>
+          </div>
+          <p className="mt-5 text-3xl font-medium">
+            {currency}
+            {productData.originalPrice}
+          </p>
+          <p className="mt-5 text-gray-500 md:w-4/5">{productData.title}</p>
+          <div className="flex flex-col gap-4 my-8">
+            <p>Select Size</p>
+            <div className="flex gap-2">
+              {productData.sizes.map((item, index) => (
+                <button
+                  onClick={() => setSize(item)}
+                  className={`border py-2 px-4 bg-gray-100 ${item === size ? "border-orange-500" : ""}`}
+                  key={index}
+                >
+                  {item}
+                </button>
+              ))}
+            </div>
+          </div>
+          <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700">
+            Add to Cart
+          </button>
+          <hr className="mt-8 sm:w-4/5" />
+          <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
+            <p>100% Original Product</p>
+            <p>The Product is available Right Now</p>
+            <p>Many Different type of Payment methods</p>
+          </div>
+        </div>
+      </div>
+      {/* Description & Review Section */}
+      <div className="mt-20">
+        <div className="flex">
+          <b className="border px-5 py-3 text-sm">Description</b>
+          <p className="border px-5 py-3 text-sm">Reveiws (122)</p>
+        </div>
+        <div className="flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem
+            suscipit beatae odit modi quisquam facere vitae fuga ad similique,
+            ipsam quasi architecto impedit unde nam dolorem corrupti nihil ipsum
+            pariatur!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+            totam recusandae unde, fuga similique necessitatibus magni
+            temporibus, doloribus reprehenderit sed cum dignissimos ducimus
+            laborum provident! Hic ducimus at atque odio.
+          </p>
         </div>
       </div>
     </div>
