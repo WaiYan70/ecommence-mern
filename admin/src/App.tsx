03e289f07a17fdbc5fb2414775874ab2f3a49399
Khant Wai Yan <52145576+WaiYan70@users.noreply.github.com>
2025-01-07 20:44:02 +0700

    Setup Admin App

---
+++ admin/src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className="bg-blue-700">
+      <div className="text-red-600">Hello Admin App</div>
+      <div className="text-black">with Raact</div>
+    </div>
+  );
+};
+
+export default App;
